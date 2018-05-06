5d566c1fb8e65fe4f5177a2d3b184599c5013543
Javier Guerrero <javierjavo@live.com.mx>
2018-05-06 18:49:23 -0500

    asdf
    asd

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -8,27 +8,25 @@ import { PopoverComponent } from '../../components/popover/popover'
   templateUrl: 'info.html'
 })
 export class InfoPage {
-
-<<<<<<< HEAD
-  postList = [];
-
-  constructor(public navCtrl: NavController, public popoverCtrl: PopoverController, public remoteService: RemoteService) {
-      this.getPosts();
-=======
   constructor(public navCtrl: NavController, public popoverCtrl: PopoverController) {
-
   }
-
-  getPosts(){
-    this.remoteService.getPosts().subscribe((data)=>{
-        this.postList = data;
-    });
-}
 
   presentPopover(myEvent) {
     let popover = this.popoverCtrl.create(PopoverComponent);
     popover.present({
       ev: myEvent
     });
+  }
+
+  ionViewDidLoad(){ 
+    var xhttp = new XMLHttpRequest(); 
+    xhttp.onreadystatechange = function() { 
+      if (this.readyState == 4 && this.status == 200) { 
+        //document.getElementById("demo").innerHTML = 
+        console.log(this.responseText); 
+      } 
+    }; 
+    xhttp.open("GET", "http://localhost/conexiondb/index.php/conexion", true); 
+    xhttp.send();
   }
 }
