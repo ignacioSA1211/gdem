9b733f3f45bba2ab4cc355027755d6f909da4d34
Javier Guerrero <javierjavo@live.com.mx>
2018-05-06 21:06:14 -0500

    Deletion of conexiondb
    ´Local conection´Removed from app path

--- conexiondb/application/models/conexion_model.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    class conexion_model extends CI_Model {
-        public function GetAll(){
-            //$query = $this->db->get('<tablename>'); selecciona la tabla a consultar
-            $query = $this->db->get('login');
-            return $query->result();
-        }
-    }
